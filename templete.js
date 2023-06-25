a5191958999720c39c7094e9e331640449dac413
ObsidianH22 <obsidian.dev.h@gmail.com>
2023-06-25 03:56:41 +0300

    Dark Modu-copyright Date-Search Data-top btn-modal

--- templete.js
+++ templete.js
@@ -1,1 +1,26 @@
-//alert("deneme")
+//Dark Mode
+
+let darkModu=()=>{
+   document.body.classList.toggle("dark_mode");
+}
+
+// input search
+$(document).ready(function(){
+   const searchApi=["Adana","Balikesir","Çorum","Denizli","Diyarbakır","Edirne","Malatya","Van"];
+   $("#tags").autocomplete({
+       source:searchApi
+   })
+   }); //end 
+
+// Footer
+let newDate=()=>{
+   const date=new Date().getFullYear();
+
+   // JS DOM
+//document.getElementById("date_id").innerHTML=new Date().getFullYear();
+
+// Jquery DOM
+$("#date_id").html(date);
+}
+newDate()
+
