82c84965d0aa6058ab91313789eadf4f36d06bf1
ObsidianH22 <obsidian.dev.h@gmail.com>
2023-07-12 02:23:16 +0300

    String

--- js/tutorials.js
+++ js/tutorials.js
@@ -156,3 +156,49 @@ console.log(parseInt("10")+10);
 */
 
 //ODEV : String() ile .toString arasındaki fark nedir?
+
+//var - let - const 
+
+// String 
+
+//boşluk bir karakter olarak kabul edilir. 
+
+/*
+let str=" js öğreniyorum js";
+console.log(str);
+console.log(str.length); //string in uzunluğunu belirtir
+console.log(str.trim().length); //boşluklar kesildikten sonraki uzunluğu verir
+
+console.log(str.toUpperCase);
+console.log(str.toLowerCase);
+
+console.log(str.charAt(0));
+
+console.log(str.indexOf("js"));
+console.log(str.lastIndexOf("js")); //sondan arayarak ifadenin indeksini bulur
+
+console.log(str.concat("sona"));
+console.log(str.replace(str , "değiştir"));
+
+console.log(str.substring(1)); //
+console.log(str.substring(0,4)); //
+
+
+*/
+
+// ÖDEV NOT:5 adımı beraber yapalım.
+// kullanıcı tarafından girilen bir kelimenin (prompt)
+// let userData= prompt("Lütfen bir şeyler yazınız");
+// S-1) Kaç karakterlidir ? (length)
+// S-2) boşluklar alınarak Kaç karakterlidir ? (trim)
+// S-3) bütün kelimeyi küçük harfle göstermek ? (toLowerCase())
+// S-4) bütün kelimeyi büyük harfle göstermek ? (toUpperCase())
+// S-5) Kullanıcıdan alınan ilk kelimeyle , "javascript" kelimeyle değiştirin. (replace, substring, indexOf)
+// console.log(userData.replace("xxxxxxx"),"javascript");
+// console.log(userData.replace(userData.substring(0,userData.indexOf(" ")),"javascript"));
+// S-6) yazdığınız kelimede "javascript ile mi başlıyor" ? (startsWith()) 
+// S-7) yazdığınız kelimede "javascript ile mi bitiyor" ? (endsWith())
+// S-8) ilk harf nedir  ? (charAt())
+// S-9) girdiğiniz kelimenin sonuna "-ben js öğreniyorum" (concat())
+// S-10) girdiğiniz 0 ile 4 arasındaki indexi ekranda gösterin? (subString())
+
