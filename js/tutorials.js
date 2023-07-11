e72e1a2e16b1a419bb04ec17b9ead88711bf790d
ObsidianH22 <obsidian.dev.h@gmail.com>
2023-07-11 18:15:54 +0300

    hoisting

--- js/tutorials.js
+++ js/tutorials.js
@@ -1,6 +1,28 @@
-console.log("test");
-console.info("info");
-console.warn("warn");
-console.error("error");
+var name="hacer"; 
+console.log(name);  // "hacer"  (konsola yazdırır ama VSCode deprecated uyarısı verir)
 
-document.writeln("test");
+var surname;
+console.log(surname);  // undefined
+
+var age;
+console.log(age);  // undefined
+age=23;
+
+job="engineer";
+console.log(job); // konsola engineer yazdırır. hoisting işlemi yaptı burada
+var job;
+
+//İlk önce değeri atadığımız için js onu bir bellekte tutuyor, “hmm varmı acaba benim aşağılarda bir yerlerde atanmış değişkenim “ diye düşünüp, sabırlı davranıp, aşağı doğru inmeye başlarken “işte buradaaa!” diye bağırıp değişkenini buluyor ve heyecanla hemen kendi değişkenini sahiplenerek onu baş tacı yapıyor.
+
+//yani yapıyı şuna çeviriyor. 
+
+/*
+var job;
+job="engineer";
+console.log(job);
+*/
+
+
+//Burada bilmemiz gereken en önemli şey, sadece değişkenler yukarı taşınır, değerler yukarı taşınmaz !
+
+//LET ve CONST hoisting işlemine duyarlı değildir !
