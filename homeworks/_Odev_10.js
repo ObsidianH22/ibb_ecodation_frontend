8387793c8e177b6a8dfbe7de8a63a73d9ba16772
ObsidianH22 <obsidian.dev.h@gmail.com>
2023-07-15 20:20:06 +0300

    3 alıştırma sorusu ve çözümü

---
+++ homeworks/_Odev_10.js
@@ -0,0 +1,38 @@
+//Örnek-1
+//y=3x+4k ==> 1.dereceden 2 bilinmeyenli denklem algoritması
+//Kullanıcı tarafından alınan x ve k değerlerini hesaplayan algoritma yazınız ?
+
+/*
+let denklem=()=> {
+    let userX=Number(prompt("X için değer giriniz..."));
+    let userK =Number(prompt("K için değer giriniz..."));
+
+    let y=(3*userX)+(4*userK);
+    console.log(y);
+}
+
+denklem()
+*/
+
+//örnek-2
+//Kullanıcı tarafından alınan dereceyi  Fahrenhayta çeviren algoritma yapalım.
+//Formül: (derece*9/5)+32
+
+/*
+
+let userData = Number( prompt("Derece bilgisi giriniz..."));
+
+let result = (userData*9/5)+32;
+
+console.log( result );
+
+*/
+
+//örnek-3 operatör işlemleri: aşağıdaki örneği javascript ile yapalım
+//4+3*2(3:3-1*6+9:1+(3:3))
+
+/*
+let result=(4+(3*2)*((3/3)-(1*6)+(9/1)+(3/3)));
+console.log(result);
+
+*/
